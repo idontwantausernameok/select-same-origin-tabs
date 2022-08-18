7acd90bf20ca468ac3cd8df73ac49c959c793d55
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-08-18 21:04:05 +0200

    fix: remove multi window, since it donest seeme very expected

--- background.js
+++ background.js
@@ -11,30 +11,17 @@ browser.menus.create({
 
         const url = new URL(tab.url);
 
-        const tabs = await browser.tabs.query({
+        const tabIdxs = (await browser.tabs.query({
             url: url.origin + "/*",
-            hidden: false
+            hidden: false,
+            currentWindow: true
+        })).map( t => t.index );
+
+        browser.tabs.highlight({
+            windowId: tab.windowId,
+            tabs: tabIdxs,
+            populate: false
         });
-
-        const winId2tabsIdx = new Map();
-
-        tabs.forEach( (t) => {
-            if(t.id !== tab.id) {
-                if(!winId2tabsIdx.has(t.windowId)){
-                    winId2tabsIdx.set(t.windowId, []);
-                }
-                winId2tabsIdx.get(t.windowId).push(t.index);
-
-            }
-        });
-
-        winId2tabsIdx.forEach( (tabsIdx, winId /*,map*/) => {
-            browser.tabs.highlight({
-                windowId: winId,
-                tabs: tabsIdx,
-                populate: false
-            });
-        });
-	}
+    }
 });
 
